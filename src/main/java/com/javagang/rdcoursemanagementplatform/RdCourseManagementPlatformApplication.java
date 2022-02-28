c749e4c1a716604fed497d1c7aedbc3e5b4dbe32
Davit Baghoyan <davit_baghoyan@epam.com>
2022-02-28 16:03:45 +0400

    added db configs, and some small fixes

--- src/main/java/com/javagang/rdcoursemanagementplatform/RdCourseManagementPlatformApplication.java
+++ src/main/java/com/javagang/rdcoursemanagementplatform/RdCourseManagementPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.JavaGang.RDcoursemanagementplatform;
+package com.javagang.rdcoursemanagementplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RdCourseManagementPlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RdCourseManagementPlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RdCourseManagementPlatformApplication.class, args);
+    }
 
 }

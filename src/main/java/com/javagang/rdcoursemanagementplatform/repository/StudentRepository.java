93ce415334b15ad0f24b54bfb1ebfeb873f2ce68
Mushegh Yeghiazaryan <mushegh_yeghiazaryan@epam.com>
2022-03-01 15:56:14 +0400

    Remove Repository annotation

--- src/main/java/com/javagang/rdcoursemanagementplatform/repository/StudentRepository.java
+++ src/main/java/com/javagang/rdcoursemanagementplatform/repository/StudentRepository.java
@@ -1,10 +1,8 @@
 package com.javagang.rdcoursemanagementplatform.repository;
 
-import org.springframework.stereotype.Repository;
 import com.javagang.rdcoursemanagementplatform.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-@Repository
 public interface StudentRepository extends JpaRepository<Student, UUID> {}

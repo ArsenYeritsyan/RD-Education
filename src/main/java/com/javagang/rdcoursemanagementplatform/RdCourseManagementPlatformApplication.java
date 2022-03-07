dc454453fe40161e1f86bdb0e115bf77da18ac0f
Davit Baghoyan <davit_baghoyan@epam.com>
2022-03-07 15:39:16 +0400

    removed unused imports

--- src/main/java/com/javagang/rdcoursemanagementplatform/RdCourseManagementPlatformApplication.java
+++ src/main/java/com/javagang/rdcoursemanagementplatform/RdCourseManagementPlatformApplication.java
@@ -1,6 +1,5 @@
 package com.javagang.rdcoursemanagementplatform;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

925bd1ffe4f26a8d1dcfc5814985544a8d3105d6
Davit Baghoyan <davit_baghoyan@epam.com>
2022-03-09 18:37:26 +0400

    CMP-4

---
+++ src/main/java/com/javagang/rdcoursemanagementplatform/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.javagang.rdcoursemanagementplatform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String Get(){
+        return "Hello";
+    }
+}

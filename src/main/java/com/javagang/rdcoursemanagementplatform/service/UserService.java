7f4e577ade1cdfd1e5e7500b4a6afc3768554ee3
Davit Baghoyan <davit_baghoyan@epam.com>
2022-03-31 16:33:22 +0400

    resolved conflicts

--- src/main/java/com/javagang/rdcoursemanagementplatform/service/UserService.java
+++ src/main/java/com/javagang/rdcoursemanagementplatform/service/UserService.java
@@ -1,9 +1,10 @@
 package com.javagang.rdcoursemanagementplatform.service;
 
-import com.javagang.rdcoursemanagementplatform.constant.Constants;
 import com.javagang.rdcoursemanagementplatform.exception.UserNotFoundException;
+import com.javagang.rdcoursemanagementplatform.mapper.UserMapper;
 import com.javagang.rdcoursemanagementplatform.model.dto.ForgotPasswordDTO;
 import com.javagang.rdcoursemanagementplatform.model.dto.ResetPasswordDTO;
+import com.javagang.rdcoursemanagementplatform.model.dto.UserDTO;
 import com.javagang.rdcoursemanagementplatform.model.entity.User;
 import com.javagang.rdcoursemanagementplatform.repository.UserRepository;
 import com.javagang.rdcoursemanagementplatform.security.JwtTokenUtil;
@@ -21,36 +22,48 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final PasswordEncoder passwordEncoder;
-  private final JwtTokenUtil jwtUtil;
-  private final MailUtility javaMailUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtUtil;
+    private final MailUtility javaMailUtil;
+    private final UserMapper mapper;
 
-  public String sendMailToResetPassword(ForgotPasswordDTO forgotPassword) {
-    User user =
-        userRepository
-            .findByMail(forgotPassword.getEmail())
-            .orElseThrow(() -> new UserNotFoundException("User Account is not found.."));
-    String jwtToken = jwtUtil.createJwtToken(user.getMail());
-    javaMailUtil.resetPasswordMail(user.getMail(), jwtToken);
-    log.info("Email has been sent to your account");
-    return user.getMail();
-  }
-
-  public void resetPassword(@Valid ResetPasswordDTO resetPassword, String token) {
-    User user =
-        userRepository
-            .findByMail(jwtUtil.verifyAndGetMail(token))
-            .orElseThrow(() -> new UserNotFoundException("User is not found.."));
-    user.setPassword(passwordEncoder.encode(resetPassword.getPassword()));
-    log.info("Password has been changed successfully");
-  }
-
-  public void sendEmailOnRegistrationSuccess (String email) {
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(email);
-    message.setSubject("Account verification mail");
-    message.setText(
-        "Registration Successful");
+    public String sendMailToResetPassword(ForgotPasswordDTO forgotPassword) {
+        User user =
+                userRepository
+                        .findByMail(forgotPassword.getEmail())
+                        .orElseThrow(() -> new UserNotFoundException("User Account is not found.."));
+        String jwtToken = jwtUtil.createJwtToken(user.getMail());
+        javaMailUtil.resetPasswordMail(user.getMail(), jwtToken);
+        log.info("Email has been sent to your account");
+        return user.getMail();
     }
+
+    public void resetPassword(@Valid ResetPasswordDTO resetPassword, String token) {
+        User user =
+                userRepository
+                        .findByMail(jwtUtil.verifyAndGetMail(token))
+                        .orElseThrow(() -> new UserNotFoundException("User is not found.."));
+        user.setPassword(passwordEncoder.encode(resetPassword.getPassword()));
+        log.info("Password has been changed successfully");
+    }
+
+    public void sendEmailOnRegistrationSuccess(String email) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Account verification mail");
+        message.setText(
+                "Registration Successful");
+    }
+
+    public UserDTO getUserByEmail(String email) {
+
+        User user = userRepository
+                .findByMail(email)
+                .orElseThrow(() -> new UserNotFoundException("User is not found.."));
+
+        return mapper.userToUserDTO(user);
+    }
+
 }
+
